26ce4ba14b2a2ddfa9ba2a5ede180a88845426bf
Jess Pesale <jess_pesale@aol.com>
2021-10-20 20:07:27 -0400

    commit tree 3d69484e1babb57ed477d94a1deee43a4d796559

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React from 'react'
+
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    componentDidMount() {
+        const url = "http://api.open-notify.org/astros.json"
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        
+    }
+
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+
+}
+
+export default App
